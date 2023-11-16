8df61d0ee5a3bb148c94873e6fe845b460626d3e
Jakyle <jjackson360@gmail.com>
2023-11-15 20:56:03 -0500

    added the ability to add an item to the shopping cart

--- point-of-sale/ShoppingCart.cs
+++ point-of-sale/ShoppingCart.cs
@@ -9,5 +9,10 @@ namespace point_of_sale
     internal class ShoppingCart
     {
         // I made a shopping cart, look at me mah!
+
+        void AddItem(string shoppingCartItem)
+        {
+            // the logic for adding a new shopping cart item.
+        }
     }
 }
